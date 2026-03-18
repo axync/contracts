c2aa15ce7683743385ec48b2b9634787fa0299e2
Alert17 <coren7986@gmail.com>
2026-03-18 23:00:59 +0100

    Global pivot

---
+++ contracts/mocks/ERC721Mock.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract ERC721Mock is ERC721 {
+    uint256 private _nextTokenId;
+
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+
+    function mint(address to) external returns (uint256 tokenId) {
+        tokenId = _nextTokenId++;
+        _mint(to, tokenId);
+    }
+}
